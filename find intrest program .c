564de9e227b831b7a04992ce32994ba77e86bda9
priyaasodariya <renukabenasodariya1@gmail.com>
2025-08-27 09:42:34 +0530

    Add files via upload

---
+++ find intrest program .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+float p ,r,n,i;
+printf("enter value of p:");
+scanf("%f",&p);
+printf("enter value ofn:");
+scanf("%f",&n);
+printf("enter value of r:");
+scanf("%f",&r);
+i=(p*r*n)/100;
+printf("intrest is:%.2f",i);
+return 0;
+}
